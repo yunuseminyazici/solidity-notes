6e301c34aef6e9a60944d439a7044e96f7c173c1
Yunus Yazici <55270091+yunuseminyazici@users.noreply.github.com>
2021-03-11 00:15:36 +0300

    Update lesson_1.sol

--- lesson_1.sol
+++ lesson_1.sol
@@ -1,11 +1,12 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-contract ZombieFactory {
-
+contract ZombieFactory {                                        //uint means unsigned integer and meaning its value must be non-negative.
+                                                                //uint is actually an alias for uint256.
+                                                                
     event NewZombie(uint zombieId, string name, uint dna);
-
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+                                                   
+    uint dnaDigits = 16;                        
+    uint dnaModulus = 10 ** dnaDigits;         
 
     struct Zombie {
         string name;
