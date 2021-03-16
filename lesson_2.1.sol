b8e022cfc0a851c64ccfe902f23d368f87ee3988
Yunus Yazici <55270091+yunuseminyazici@users.noreply.github.com>
2021-03-16 23:41:07 +0300

    Update lesson_2.1.sol

--- lesson_2.1.sol
+++ lesson_2.1.sol
@@ -12,11 +12,34 @@ contract ZombieFactory {
         uint dna;
     }
 
-    Zombie[] public zombies;
+    Zombie[] public zombies;                                        // For a financial app, storing a uint that holds the user's account balance:
+                                                                           // mapping (address => uint) public accountBalance;
+                                                                    // Or could be used to store / lookup usernames based on userId
+                                                                           // mapping (uint => string) userIdToName;
 
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
+            
+                                                                  // In Solidity, there are certain global variables that are available to all functions.
+                                                                  // One of these is msg.sender, which refers to the address of the person (or smart contract) who called the current function.                                                                
+                                                                  
+                                                                  //Note: In Solidity, function execution always needs to start with an external caller.
+                                                                  //A contract will just sit on the blockchain doing nothing until someone calls one of its functions. So there will always be a msg.sender.
+        
+ */ mapping (address => uint) favoriteNumber;
 
+function setMyNumber(uint _myNumber) public {
+  // Update our `favoriteNumber` mapping to store `_myNumber` under `msg.sender`
+  favoriteNumber[msg.sender] = _myNumber;
+  // ^ The syntax for storing data in a mapping is just like with arrays
+}
+
+function whatIsMyNumber() public view returns (uint) {
+  // Retrieve the value stored in the sender's address
+  // Will be `0` if the sender hasn't called `setMyNumber` yet
+  return favoriteNumber[msg.sender];
+} */
+        
     function _createZombie(string memory _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
