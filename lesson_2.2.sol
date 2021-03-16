0fbdca24616304caab0c9365222927e67eec7a28
Yunus Yazici <55270091+yunuseminyazici@users.noreply.github.com>
2021-03-17 00:24:01 +0300

    Update lesson_2.2.sol

--- lesson_2.2.sol
+++ lesson_2.2.sol
@@ -1,6 +1,56 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "./zombiefactory.sol";
+import "./zombiefactory.sol"; //When you have multiple files and you want to import one file into another, Solidity uses the import keyword:
+
+/*
+In Solidity, there are two locations you can store variables — in storage and in memory.
+
+Storage refers to variables stored permanently on the blockchain. 
+Memory variables are temporary, and are erased between external function calls to your contract. 
+Think of it like your computer's hard disk vs RAM.
+*/
+
+/*
+contract SandwichFactory {
+  struct Sandwich {
+    string name;
+    string status;
+  }
+
+  Sandwich[] sandwiches;
+
+  function eatSandwich(uint _index) public {
+    // Sandwich mySandwich = sandwiches[_index];
+
+    // ^ Seems pretty straightforward, but solidity will give you a warning
+    // telling you that you should explicitly declare `storage` or `memory` here.
+
+    // So instead, you should declare with the `storage` keyword, like:
+    Sandwich storage mySandwich = sandwiches[_index];
+    // ...in which case `mySandwich` is a pointer to `sandwiches[_index]`
+    // in storage, and...
+    mySandwich.status = "Eaten!";
+    // ...this will permanently change `sandwiches[_index]` on the blockchain.
+
+    // If you just want a copy, you can use `memory`:
+    Sandwich memory anotherSandwich = sandwiches[_index + 1];
+    // ...in which case `anotherSandwich` will simply be a copy of the 
+    // data in memory, and...
+    anotherSandwich.status = "Eaten!";
+    // ...will just modify the temporary variable and have no effect 
+    // on `sandwiches[_index + 1]`. But you can do this:
+    sandwiches[_index + 1] = anotherSandwich;
+    // ...if you want to copy the changes back into blockchain storage.
+  }
+*/
+
+/*
+Notice that this looks like defining a contract, with a few differences. For one, we're only declaring the functions we want to interact with — in this case getKitty — and we don't mention any of the other functions or state variables.
+
+Secondly, we're not defining the function bodies. Instead of curly braces ({ and }), we're simply ending the function declaration with a semi-colon (;).
+
+So it kind of looks like a contract skeleton. This is how the compiler knows it's an interface.
+*/
 
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
@@ -20,7 +70,7 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract = KittyInterface(ckAddress);  //In this way, your contract can interact with any other contract on the Ethereum blockchain, as long they expose those functions as public or external.
 
   
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
@@ -29,7 +79,7 @@ contract ZombieFeeding is ZombieFactory {
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
 
-    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {    
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
@@ -37,7 +87,7 @@ contract ZombieFeeding is ZombieFactory {
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
-    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);  //getKitty returns a ton of variables. But all we care about is the last one, genes.
     
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
