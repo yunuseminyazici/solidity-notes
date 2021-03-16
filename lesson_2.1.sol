1373ff2b2ea65994357280b1e59311e7fd8a7401
Yunus Yazici <55270091+yunuseminyazici@users.noreply.github.com>
2021-03-16 23:57:18 +0300

    Update lesson_2.1.sol

--- lesson_2.1.sol
+++ lesson_2.1.sol
@@ -52,6 +52,16 @@ function whatIsMyNumber() public view returns (uint) {
         return rand % dnaModulus;
     }
 
+*/ 
+   function sayHiToVitalik(string memory _name) public returns (string memory) {
+  // Compares if _name equals "Vitalik". Throws an error and exits if not true.
+  // (Side note: Solidity doesn't have native string comparison, so we
+  // compare their keccak256 hashes to see if the strings are equal)
+  require(keccak256(abi.encodePacked(_name)) == keccak256(abi.encodePacked("Vitalik")));
+  // If it's true, proceed with the function:
+  return "Hi!";
+}
+*/
     function createRandomZombie(string memory _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
